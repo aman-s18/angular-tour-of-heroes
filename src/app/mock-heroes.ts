29277cdb58bc17933386f0670e058ec76878ec74
Aman Sharma <aman.ammu593@gmail.com>
2020-09-16 21:20:52 +0530

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 7, name: 'Mahendra Singh Dhoni', title: 'CSK' },
+  { id: 18, name: 'Virat Kolhi', title: 'RCB' },
+  { id: 45, name: 'Ro-Hit Sharma',title: 'MI' },
+  { id: 17, name: 'Rishabh Pant',title: 'DC' },
+  { id: 21, name: 'Dinesh Kartik' ,title: 'KKR'},
+  { id: 3, name: 'Suresh Raina' ,title: 'CSK'},
+  { id: 47, name: 'DJ Bravo' ,title: 'CSK'},
+  { id: 90, name: 'Deepak Chahar' ,title: 'CSK'},
+  { id: 5, name: 'Aaron Finch' ,title: 'RCB'},
+  { id: 17, name: 'AB deviliears ',title: 'RCB' }
+];
